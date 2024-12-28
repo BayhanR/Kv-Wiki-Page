3ccf7100dc2c3e88e28d0b64022b47b8989c73f1
Mustafa Kemal Gordesli <m.kemalgordesli@gmail.com>
2024-12-28 20:18:29 +0300

    Characterler api'dan çekildi

--- kurtlar-vadisi-wiki/src/pages/Karakterler.tsx
+++ kurtlar-vadisi-wiki/src/pages/Karakterler.tsx
@@ -1,10 +1,23 @@
 // src/pages/Karakterler.tsx
-import React from 'react';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 import CharacterGrid from "../components/CharacterGrid";
 const Karakterler = () => {
+  const [characters, SetCharacters] = useState([])
+
+  useEffect(() => {
+    getCharecters()
+  }, [])
+
+  const getCharecters = () => {
+    axios.get("https://api.themoviedb.org/3/tv/34587/credits?api_key=3b3b9c816ffa9292cbfb27de2b1ffe58")
+      .then(res => SetCharacters(res.data.cast))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div>
-      <CharacterGrid />
+      <CharacterGrid characters={characters}/>
     </div>
   );
 }
